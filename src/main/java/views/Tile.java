32159aa31961c05c2a16ec0eb5f04643b86f7102
Victor <victor.sapuyes@correounivalle.edu.co>
2022-07-04 20:49:15 -0500

    changes to jar

--- src/main/java/views/Tile.java
+++ src/main/java/views/Tile.java
@@ -4,43 +4,40 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.util.Objects;
 
 
 public class Tile extends StackPane {
 
-    private final Image premio1 = new Image(new FileInputStream("src/main/resources/images/premio1.png"));
-
-    public Tile(Integer color) throws FileNotFoundException {
+    public Tile(Integer color) {
         ImageView imageView = new ImageView();
         switch (color) {
             case 0 -> {
-                Image free = new Image(new FileInputStream("src/main/resources/images/free.png"));
+                Image free = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/free.png")));
                 imageView.setImage(free);
             }
             case 1 -> {
-                Image wall = new Image(new FileInputStream("src/main/resources/images/wall.png"));
+                Image wall = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/wall.png")));
                 imageView.setImage(wall);
             }
             case 2 -> {
-                Image bmo = new Image(new FileInputStream("src/main/resources/images/bmo.png"));
+                Image bmo = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/bmo.png")));
                 imageView.setImage(bmo);
             }
             case 3 -> {
-                Image ship = new Image(new FileInputStream("src/main/resources/images/ship.png"));
+                Image ship = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/ship.png")));
                 imageView.setImage(ship);
             }
             case 4 -> {
-                Image ship2 = new Image(new FileInputStream("src/main/resources/images/ship2.png"));
+                Image ship2 = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/ship2.png")));
                 imageView.setImage(ship2);
             }
             case 5 -> {
-                Image premio2 = new Image(new FileInputStream("src/main/resources/images/premio2.png"));
-                imageView.setImage(premio2);
+                Image reward = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/reward.png")));
+                imageView.setImage(reward);
             }
             case 6 -> {
-                Image oil = new Image(new FileInputStream("src/main/resources/images/oil.png"));
+                Image oil = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/oil.png")));
                 imageView.setImage(oil);
             }
         }
